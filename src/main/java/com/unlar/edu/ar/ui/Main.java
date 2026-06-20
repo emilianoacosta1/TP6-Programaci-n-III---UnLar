e1b49ad5926bcd6fb318895dcc37d51b6f9e2e7f
Emiliano Acostae <emilianoav0205@gmail.com>
2026-06-20 00:50:30 -0300

    Ejercicio 1 y Pregunta 1 Resuelta

---
+++ src/main/java/com/unlar/edu/ar/ui/Main.java
@@ -0,0 +1,29 @@
+package com.unlar.edu.ar.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.unlar.edu.ar.model.Estudiante;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+
+        // --- PRUEBA PARTE 1 ---
+        List<Estudiante> lista = new ArrayList<>();
+        
+        lista.add(new Estudiante("LU-2024-001", "Martín Quiroga", 8.5, 22, 18));
+        lista.add(new Estudiante("LU-2024-002", "Valeria Díaz", 8.5, 20, 15));
+        lista.add(new Estudiante("LU-2024-003", "Facundo Castro", 7.2, 24, 22));
+        lista.add(new Estudiante("LU-2024-004", "Camila Torres", 9.1, 21, 24));
+        lista.add(new Estudiante("LU-2024-006", "Agustina López", 6.8, 19, 10));
+        
+        Collections.sort(lista);
+    }
+}
